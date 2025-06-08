a405b720dc021e9e20bb86012191942b0b8612eb
Aditya Das <128395061+Aditya-0911@users.noreply.github.com>
2025-06-08 22:11:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
 # Load Gemini API key
-genai.configure(api_key="AIzaSyDjo7ZQa9CQ65G7kUtcZ1VFl34wkyt0U7M")
+genai.configure(api_key="Add your API key")
 
-WKHTMLTOPDF_PATH = r"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"
+WKHTMLTOPDF_PATH = "Add your path"
